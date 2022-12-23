57f3de5980ca90ebaa44b0eb099333c2beb7e764
Daniel Ojeda <dmojeda@utpl.edu.ec>
2022-12-23 11:14:20 -0500

    trabajo grupal por Daniel Ojeda y Richard Ortiz

--- Taller10/src/paquete2/Problema02.java
+++ Taller10/src/paquete2/Problema02.java
@@ -1,14 +1,39 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
+ * To change this license header, choose License Headers in Project Propertines.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package paquete2;
-
+import java.util.Locale;
+import java.util.Scanner;
 /**
  *
  * @author reroes
  */
 public class Problema02 {
-    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        String[] vendedores = {"Jessica Cole", "Robert Wallace"};
+        int[][] venta = new int[2][5];
+        String reporte = "";
+        int ventaDia;
+        int suma = 0;
+
+        for (int i = 0; i < venta.length; i++) {
+            System.out.printf("Ingrese las ventas de: %s\n\n",vendedores[i]);
+            for (int j = 0; j < venta[i].length; j++) {
+                System.out.printf("Ingrese el numero de ventas del dia\n");
+                ventaDia = entrada.nextInt();
+                suma = suma + ventaDia;
+                ventaDia = venta[i][j];
+
+            }
+        }
+        System.out.print("-----------------------------------------------\n");
+        System.out.printf("Vendedor(a) %s\nVendedor(a) %s\nHa realizado un total"
+                + " de %d en ventas.\n1", vendedores[0], vendedores[1], suma);
+        System.out.print("-----------------------------------------------\n");
+    }
 }
--- Taller10/src/paquete5/Problema05.java
+++ Taller10/src/paquete5/Problema05.java
@@ -5,10 +5,53 @@
  */
 package paquete5;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Problema05 {
-    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*
+        Antes de analizar el presente ejercicio,
+        usted debe revisar y entender lo desarrollado
+        en el archivo Ayuda01.java del paquete2
+         */
+        Scanner entrada = new Scanner(System.in);
+        String[][] estudiantes =    {{"Sarah Ward", "Philip Payne"},
+                                    {"Carrie Burton", "Lauren Rice"},
+                                    {"Emma Escobar", "Lori Flores"},
+                                    {"Steven West", "Toni Martin"}
+                                    };
+        String inicial;
+        char inicialLetra;
+        boolean bandera = true;
+        String opcion;
+        while (bandera) {
+            System.out.println("Ingrese una letra");
+            inicial = entrada.nextLine();
+            
+            
+            for (int i = 0; i < estudiantes.length; i++) {
+                
+                inicialLetra = estudiantes[i].charAt(0);
+                String inicialDos = String.valueOf(inicialLetra);
+                
+                if (inicial.equals(inicialDos)) {
+                    
+                    bandera = false;
+                    
+                }
+            }
+            
+           
+        }
+
+    }
+
 }
